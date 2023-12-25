5865c280b7fc0e222bdb9f53ba231dc51beabfbb
Sergey Lebedev <lebedev2406@yandex.ru>
2023-12-25 16:14:01 +0300

    added task2

--- Program.cs
+++ Program.cs
@@ -1,23 +1,11 @@
 ﻿
-//тип_данных имя_переменной = значение;
-// camelCase: firstNumber, secondNumber
 
-int number = 9; // number = 9
-int result = number * number; //81 
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = N * -1;
 
-// Console.ReadLine() - получить информацию из строчки
-
-Console.Write("Введите число: "); //a
-int firstNumber = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите число: "); //b
-int secondNumber = Convert.ToInt32(Console.ReadLine());
-
-if (firstNumber == secondNumber * secondNumber)
+while (negativeN <= N)
 {
-    Console.WriteLine("Да, " + firstNumber +"является кв. от числа" + secondNumber);
-}
-else 
-{
-    Console.WriteLine("Нeт, " + firstNumber +"не является кв. от числа" + secondNumber);
+    Console.Write(negativeN +" ");
+    negativeN++;
 }
